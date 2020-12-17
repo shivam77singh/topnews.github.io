44d6123e69011b37bc59c68462f0c88413918f0e
shivam77singh <47072675+shivam77singh@users.noreply.github.com>
2020-12-17 20:30:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 
 
-
 let CAT = ["entertainment", "science", "sports", "technology", "health", "business"];
 let btns = document.getElementsByClassName('btn');
 let prev;
@@ -25,9 +24,10 @@ for (let i = 0; i < 6; i++) {
         // document.querySelector('.head').style.margin = '5rem';
         let xhr = new XMLHttpRequest();
 
+        // to get updated news
+        // let url = `http://newsapi.org/v2/top-headlines?country=in&category=${CAT[i]}&apiKey=3804253855244010b346e6028aa9cb41`;
 
-
-        xhr.open('GET', `http://newsapi.org/v2/top-headlines?country=in&category=${CAT[i]}&apiKey=3804253855244010b346e6028aa9cb41`, true);
+        xhr.open('GET', `json/${CAT[i]}.json`, true);
 
         // xhr.onprogress = function () {
         //     console.log('progress');
@@ -42,36 +42,153 @@ for (let i = 0; i < 6; i++) {
                 for (key in info) {
                     // console.log('inside for loop');
                     str += `
-                    <div class="news">
-                      <div class="author-title">
-                        <span class="author">By__${info[key].author}</span>
-                        <span class="title">${info[key].title}</span>
-                      </div>
-                      <div class="news-image">
-                        <img class="image" src="${info[key].urlToImage}">
-                      </div>
-                      <div class="content">
-                        <p class="main-content">${info[key].content}
-                        <a class="link" href="${info[key].url}">Read More Here</a>
-                        </p>
-                       </div>
+                    <div class="news-container">
+                        <div class="news">
+                            <div class="author-title">
+                               <span class="author">By__${info[key].author}</span>
+                               <span class="title">${info[key].title}</span>
+                            </div>
+                            <div class="news-image">
+                               <img class="image" src="${info[key].urlToImage}">
+                            </div>
+                            <div class="content">
+                                <p class="main-content">${info[key].content}
+                                    <a class="link" href="${info[key].url}">Read More Here</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="bookmark">
+                           <i class="fas fa-bookmark"></i>
+                           <span class="bk">Bookmark</span>
+                        </div>
                     </div>
+            
                      `;
                 }
-
-                // console.log(info);
 
                 let lists = document.querySelector('.news-collection');
                 lists.innerHTML = str;
 
+                // console.log(info);
 
                 let news = document.getElementsByClassName('news');
 
+
+
+
+
+                //bookmark animation
+
+                let bookmark = document.getElementsByClassName('bookmark');
+                let cart_container = document.querySelector('.cart-container');
+                // console.log(bookmark);
+
+
+                for (let key = 0; key < bookmark.length; key++) {
+                    // console.log('ppp');
+                    bookmark[key].addEventListener('click', () => {
+                        // let str = `
+                        //  <div class="cart-items">
+                        //     <div class="cart-title">
+                        //         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        //             Deserunt sequi laboriosam</p>
+                        //         <div class="line"></div>
+                        //         </div>
+                        //      <div class="trash">
+                        //           <i class="fa fa-trash"></i>
+                        //      </div>
+                        // </div>`;
+
+                        //cart-items DIV
+                        let cart_items = document.createElement('div');
+                        cart_items.className = 'cart-items';
+
+                        //cart-title DIV
+                        let nodes = news[key].childNodes[1];
+                        let cart_title = document.createElement('div');
+                        cart_title.className = 'cart-title';
+                        cart_title.innerText = nodes.childNodes[3].innerText;
+
+                        //cart-title p
+                        let cart_title_p = document.createElement('p');
+
+                        //line DIV
+                        let line = document.createElement('div');
+                        line.className = 'line';
+
+                        //trash DIV
+
+                        let trash = document.createElement('div');
+                        trash.className = 'trash';
+                        trash.innerHTML = `<i class="fa fa-trash"></i>`;
+
+                        cart_title.appendChild(cart_title_p);
+                        cart_title.appendChild(line);
+
+                        cart_items.appendChild(cart_title);
+                        cart_items.appendChild(trash);
+
+                        cart_container.appendChild(cart_items);
+
+
+                        document.querySelector('.cart-container').style.right = '0px';
+
+
+                        
+
+
+                        // delete items of bookmarked list
+
+                     let trash_delete = document.getElementsByClassName('trash');
+                    //  let cart_items = document.getElementsByClassName('cart-items');
+                     for (let i = 0; i < trash_delete.length; i++) {
+                         console.log(i);
+                         console.log(trash_delete[i].parentElement);
+                         trash_delete[i].addEventListener('click', () => {
+                             trash_delete[i].parentElement.remove();
+                         });
+                     }
+
+
+
+                    });
+
+
+                    
+
+
+                }
+
+                // bookmark animation ends here
+
+
+
+
+
+
+
+
+
+
+                let news_container = document.getElementsByClassName('news-container');
+
                 for (let key = 0; key <= news.length; key++) {
+
+
+                    news_container[key].addEventListener('mouseover', () => {
+                        document.getElementsByClassName('bookmark')[key].style.right = '0';
+                        document.getElementsByClassName('bookmark')[key].style.opacity = '1';
+                    });
+
+                    news[key].addEventListener('mouseout', () => {
+                        document.getElementsByClassName('bookmark')[key].style.right = '-100px';
+                        document.getElementsByClassName('bookmark')[key].style.opacity = '0';
+                    });
+
                     news[key].addEventListener('click', () => {
                         // console.log(news[key]);
                         let nodes = news[key].childNodes;
-                        console.log(nodes);
+                        // console.log(nodes);
                         document.querySelector('.active-news-container').style.display = 'flex';
                         //author-title
                         let author_title = nodes[1];
@@ -108,7 +225,14 @@ for (let i = 0; i < 6; i++) {
 
                         });
                     });
+                    news[key].addEventListener('onmouseover', () => {
+                        // console.log('ooo');
+                        document.getElementsByClassName('bookmark')[0].classList.add('jjj');
+                    });
                 }
+
+
+
 
 
 
@@ -125,14 +249,19 @@ for (let i = 0; i < 6; i++) {
 
     });
 
+
+
+
     window.addEventListener('scroll', () => {
-        if (window.scrollY >= 300) {
+
+        let dist = window.scrollY;
+        if (dist >= 300) {
             document.getElementsByClassName('btns')[0].classList.add('side-btns');
-            document.getElementsByClassName('news-page')[0].style.width = '80%';
+            document.getElementsByClassName('news-page')[0].style.width = '100%';
             document.getElementsByClassName('news-collection')[0].classList.add('side-news');
 
             //animating buttons
-            
+
 
         }
         else {
@@ -140,11 +269,50 @@ for (let i = 0; i < 6; i++) {
             document.getElementsByClassName('news-page')[0].style.width = '100%';
             document.getElementsByClassName('news-collection')[0].classList.remove('side-news');
         }
+
+
+
+
     });
 
 }
 
 // side buttons
+
+
+
+//hiding bookmark container
+
+
+
+
+//open bookmark container
+
+document.querySelector('.top-bookmark').addEventListener('click', () => {
+    console.log('uyt');
+    console.log(document.querySelector('.cart-container').style.right);
+    if(document.querySelector('.cart-container').style.right === '-500px'){
+        document.querySelector('.top-bookmark').style.display = 'none';
+        document.querySelector('.cart-container').style.right = '0px';
+    }
+    else{
+        document.querySelector('.cart-container').style.right = '-500px';
+        document.querySelector('.top-bookmark').style.display = 'flex';
+    }
+});
+
+//hiding bookmark container
+
+document.querySelector('.fa-times').addEventListener('click', () => {
+    if(document.querySelector('.cart-container').style.right === '0px'){
+        document.querySelector('.cart-container').style.right = '-500px';
+        document.querySelector('.top-bookmark').style.display = 'flex';
+    }
+    else {
+        document.querySelector('.cart-container').style.right = '0px';
+    }
+});
+
 
 
 
@@ -174,6 +342,10 @@ let str = "";
     }
 
     document.querySelector('.text').innerHTML = str;
-    setTimeout(add_effect, 200);
+    setTimeout(add_effect, 170);
 
 })();
+
+
+
+
