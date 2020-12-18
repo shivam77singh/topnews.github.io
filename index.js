611a0a087e1f7cad7c632190f15116d95d046290
shivam77singh <47072675+shivam77singh@users.noreply.github.com>
2020-12-18 20:32:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -83,14 +83,32 @@ for (let i = 0; i < 6; i++) {
                 let cart_container = document.querySelector('.cart-container');
                 // console.log(bookmark);
 
+                let bookmark_list = [];
+
+
+                let news_key_bookmark_title_pair = {};
 
                 for (let key = 0; key < bookmark.length; key++) {
                     // console.log('ppp');
                     bookmark[key].addEventListener('click', () => {
+
+                        function check(x) {
+                            return x === key;
+                        }
+                        let f = 0;
+
+                        if (bookmark_list.length && bookmark_list.find(check) === key) {
+                            console.log('Already in the List');
+                            alert('Already in the List');
+                            f = 1;
+                        }
+                        bookmark_list.push(key);
+
+                        // console.log(bookmark[key], 'key');
                         // let str = `
                         //  <div class="cart-items">
                         //     <div class="cart-title">
-                        //         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        //         <p class="cart-title-p">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                         //             Deserunt sequi laboriosam</p>
                         //         <div class="line"></div>
                         //      </div>
@@ -100,63 +118,148 @@ for (let i = 0; i < 6; i++) {
                         // </div>`;
 
                         //cart-items DIV
-                        let cart_items = document.createElement('div');
-                        cart_items.className = 'cart-items';
+                        if (!f) {
+                            let cart_items = document.createElement('div');
+                            cart_items.className = `cart-items ${key}`;
 
-                        //cart-title DIV
-                        let nodes = news[key].childNodes[1];
-                        let cart_title = document.createElement('div');
-                        cart_title.className = 'cart-title';
-
-
-                        //cart-title p
-                        let cart_title_p = document.createElement('p');
-                        cart_title_p.innerText = nodes.childNodes[3].innerText;
-
-                        //line DIV
-                        let line = document.createElement('div');
-                        line.className = 'line';
-
-                        //trash DIV
-
-                        let trash = document.createElement('div');
-                        trash.className = 'trash';
-                        trash.innerHTML = `<i class="fa fa-trash"></i>`;
-
-                        cart_title.appendChild(cart_title_p);
-                        cart_title.appendChild(line);
-
-                        cart_items.appendChild(cart_title);
-                        cart_items.appendChild(trash);
-
-                        cart_container.appendChild(cart_items);
+                            //cart-title DIV
+                            let nodes = news[key].childNodes[1];
+                            let cart_title = document.createElement('div');
+                            cart_title.className = 'cart-title';
 
 
-                        document.querySelector('.cart-container').style.right = '0px';
-                        //document.getElementsByClassName('cart-items').style.opacity = '1';
+                            //cart-title p
+                            let cart_title_p = document.createElement('p');
+                            cart_title_p.className = 'cart-title-p';
+                            cart_title_p.innerText = nodes.childNodes[3].innerText;
 
-                        // cart_items.right = '-100px';
+                            //adding to news_key_bookmark_title_pair list
+                            let t = news[key].childNodes[1].childNodes[3].innerText;
+                            news_key_bookmark_title_pair['t'] = news[key];
+
+                            // console.log(news_key_bookmark_title_pair['t'], t);
+                            //line DIV
+                            let line = document.createElement('div');
+                            line.className = 'line';
+
+                            //trash DIV
+
+                            let trash = document.createElement('div');
+                            trash.className = 'trash';
+                            trash.innerHTML = `<i class="fa fa-trash"></i>`;
+
+                            cart_title.appendChild(cart_title_p);
+                            cart_title.appendChild(line);
+
+                            cart_items.appendChild(cart_title);
+                            cart_items.appendChild(trash);
+
+                            cart_container.appendChild(cart_items);
+
+
+                            document.querySelector('.cart-container').style.right = '0px';
+                            //document.getElementsByClassName('cart-items').style.opacity = '1';
+
+                            // cart_items.right = '-100px';
 
 
 
-                        // delete items of bookmarked list
+                            // delete items of bookmarked list
 
-                        let trash_delete = document.getElementsByClassName('fa-trash');
-                        //  let cart_items = document.getElementsByClassName('cart-items');
+                            let trash_delete = document.getElementsByClassName('fa-trash');
+                            //  let cart_items = document.getElementsByClassName('cart-items');
 
-                        for (let i = 0; i < trash_delete.length; i++) {
+                            for (let i = 0; i < trash_delete.length; i++) {
 
-                            //  console.log(i);
-                            // console.log(trash_delete[key].parentElement);
-                            // let x = document.getElementsByClassName('cart-items')[i];
-                            trash_delete[i].addEventListener(('click'), (e) => { 
-                                console.log( e.target.parentElement);
-                                // e.target.parentElement.remove();
-                                console.log(e.target);
-                                e.target.parentElement.parentElement.remove();
-                            });
+
+                                trash_delete[i].addEventListener(('click'), (e) => {
+
+
+                                    //delete animation
+                                    let parent = e.target.parentElement.parentElement;
+
+                                    console.log(typeof (parent.classList[1]), 'parent');
+                                    for (let k = 0; k < 100; k++) {
+
+                                        if (parent.classList[1] === k.toString()) {
+                                            console.log(parent.classList, 'classsss');
+                                            delete bookmark_list[bookmark_list.indexOf(k)];
+                                        }
+                                    }
+
+                                    parent.classList.add('delete-animation');
+
+                                    parent.addEventListener('transitionend', () => {
+                                        parent.remove();
+                                    });
+
+
+                                    if (trash_delete.length === 1) {
+                                        setTimeout(() => {
+                                            if (document.querySelector('.cart-container').style.right === '0px') {
+                                                document.querySelector('.cart-container').style.right = '-500px';
+                                            }
+                                        }, 700);
+
+                                    }
+
+                                });
+                            }
+
+
+                            console.log(document.getElementsByClassName('cart-items'), 'vscds');
+
+                            let to_show_items = document.getElementsByClassName('cart-items');
+                            // console.log(to_show_items);
+
+                            for (let i = 0; i < to_show_items.length; i++) {
+                                to_show_items[i].childNodes[0].childNodes[0].addEventListener('click', (e) => {
+                                    let key = parseInt(to_show_items[i].classList[1]);
+
+                                    let nodes = news[key].childNodes;
+
+                                    document.querySelector('.active-news-container').style.display = 'flex';
+                                    //author-title
+                                    let author_title = nodes[1];
+
+                                    //active-author
+                                    let active_author = document.querySelector('.active-author');
+                                    active_author.innerText = author_title.childNodes[1].innerText;
+
+                                    //active title
+                                    let active_title = document.querySelector('.active-title');
+                                    active_title.innerText = author_title.childNodes[3].innerText;
+
+                                    //active-content
+                                    let content = nodes[5];
+                                    let active_content = document.querySelector('.active-content');
+                                    active_content.innerText = content.childNodes[1].innerText;
+
+                                    //active-image
+                                    let news_image = nodes[3];
+                                    let image_src = news_image.childNodes[1].currentSrc;
+                                    let elem = document.createElement('img');
+                                    elem.src = image_src;
+                                    elem.className = 'active-image';
+                                    document.getElementById('img').appendChild(elem);
+
+                                    let cnt = document.getElementById('img');
+
+                                    let page = document.getElementsByClassName('active-news-container');
+                                
+                                    window.addEventListener('click', function (e) {
+                                        if (e.target === page[0]) {
+                                            page[0].style.display = 'none';
+                                            elem.remove();
+                                        }
+    
+    
+                                    });
+                                });
+
+                               
+                            }
                         }
-
 
 
                     });
@@ -170,9 +273,9 @@ for (let i = 0; i < 6; i++) {
                 // bookmark animation ends here
 
 
+                //popp on clicking cart itmes;
 
-
-
+                //   
 
 
 
@@ -205,6 +308,7 @@ for (let i = 0; i < 6; i++) {
                         let active_author = document.querySelector('.active-author');
                         active_author.innerText = author_title.childNodes[1].innerText;
 
+                        //active title
                         let active_title = document.querySelector('.active-title');
                         active_title.innerText = author_title.childNodes[3].innerText;
 
@@ -233,16 +337,19 @@ for (let i = 0; i < 6; i++) {
 
                         });
                     });
-                    news[key].addEventListener('onmouseover', () => {
-                        // console.log('ooo');
-                        document.getElementsByClassName('bookmark')[0].classList.add('jjj');
-                    });
+
+
                 }
 
 
 
 
 
+
+
+
+
+                //ok status ends here
 
             }
             else {
@@ -300,12 +407,10 @@ document.querySelector('.top-bookmark').addEventListener('click', () => {
     console.log('uyt');
     console.log(document.querySelector('.cart-container').style.right);
     if (document.querySelector('.cart-container').style.right === '-500px') {
-        document.querySelector('.top-bookmark').style.display = 'none';
         document.querySelector('.cart-container').style.right = '0px';
     }
     else {
         document.querySelector('.cart-container').style.right = '-500px';
-        document.querySelector('.top-bookmark').style.display = 'flex';
     }
 });
 
@@ -314,12 +419,12 @@ document.querySelector('.top-bookmark').addEventListener('click', () => {
 document.querySelector('.fa-times').addEventListener('click', () => {
     if (document.querySelector('.cart-container').style.right === '0px') {
         document.querySelector('.cart-container').style.right = '-500px';
-        document.querySelector('.top-bookmark').style.display = 'flex';
     }
     else {
         document.querySelector('.cart-container').style.right = '0px';
     }
 });
+
 
 
 
